d03cf5fee85057a17ac787cc4f526b562df432af
PaunPrashant <paunprashant5@gmail.com>
2025-05-31 20:42:10 +0530

    prashant-update
    - removed unnecessary code

--- app/page.jsx
+++ app/page.jsx
@@ -5,13 +5,6 @@ import Hero1 from "@/components/homes/home-1/heros/Hero1";
 import Header1Multipage from "@/components/headers/Header1Multipage";
 import { menuItems } from "@/data/menu";
 
-// export const metadata = {
-//   title:
-//     "Preview Page || Resonance &mdash; One & Multi Page React Nextjs Creative Template",
-//   description:
-//     "Resonance &mdash; One & Multi Page React Nextjs Creative Template",
-// };
-
 export const metadata = {
   title:
     "Qubent Solutions – Intent-Driven IT | Design with Empathy, Code with Clarity",
@@ -43,74 +36,3 @@ export default function Home() {
   </div>
 }
 
-// export default function Home1() {
-//   return (
-//     <>
-//       <div className="theme-main">
-//         <div className="page" id="top">
-//           {/* Navigation Panel */}
-//           <nav className="main-nav transparent stick-fixed wow-menubar">
-//             <HeaderPreview />
-//           </nav>
-//           {/* End Navigation Panel */}
-//           <main id="main">
-//             {/* Home Section */}
-//             <ParallaxContainer
-//               className="home-section bg-gray-light-1 bg-light-alpha-90 parallax-5 parallax-mousemove-scene scrollSpysection"
-//               style={{
-//                 backgroundImage:
-//                   "url(/assets/images/full-width-images/section-bg-1.jpg)",
-//               }}
-//               id="home"
-//             >
-//               <Hero />
-//             </ParallaxContainer>
-//             {/* End Home Section */}
-//             {/* Showcases Section */}
-//             <section className="full-wrapper mt-n90">
-//               <Showcase />
-//             </section>
-//             {/* End Showcases Section */}
-//             {/* Multi Page Demos Section */}
-//             <section className="page-section overflow-hidden" id="multi-page">
-//               <Multipage />
-//             </section>
-//             {/* End Multi Page Demos Section */}
-//             {/* Divider */}
-//             <hr className="mt-0 mb-0" />
-//             {/* End Divider */}
-//             {/* One Page Demos Section */}
-//             <section className="page-section overflow-hidden" id="one-page">
-//               <Onepage />
-//             </section>
-//             {/* End One Page Demos Section */}
-//             {/* Divider */}
-//             <hr className="mt-0 mb-0" />
-//             {/* End Divider */}
-//             {/* Intro Sections Demos Section */}
-//             <section
-//               className="page-section overflow-hidden"
-//               id="intro-sections"
-//             >
-//               <Intro />
-//             </section>
-//             {/* End Intro Sections Demos Section */}
-//             {/* Divider */}
-//             <hr className="mt-0 mb-0" />
-//             {/* End Divider */}
-//             {/* Testimonials Section */}
-//             <section className="page-section">
-//               <Testomonials />
-//             </section>
-//             {/* End Testimonials Section */}
-//           </main>
-//           {/* Footer */}
-//           <footer className="page-section footer bg-gray-light-1 pb-30">
-//             <FooterPreview />
-//           </footer>
-//           {/* End Footer */}
-//         </div>
-//       </div>
-//     </>
-//   );
-// }
--- components/homes/home-1/index.jsx
+++ components/homes/home-1/index.jsx
@@ -4,7 +4,6 @@ import Benefits from "@/components/homes/home-1/Benefits";
 import Blog from "@/components/homes/home-1/Blog";
 import Brands from "@/components/homes/home-1/Brands";
 import Contact from "@/components/homes/home-1/Contact";
-
 import Facts from "@/components/homes/home-1/Facts";
 import Faq from "@/components/homes/home-1/Faq";
 import Features from "@/components/homes/home-1/Features";
@@ -12,12 +11,10 @@ import NewsLetter from "@/components/homes/home-1/NewsLetter";
 import Portfolio from "@/components/homes/home-1/Portfolio";
 import Promo from "@/components/homes/home-1/Promo";
 import Service from "@/components/homes/home-1/Service";
-import Team from "@/components/homes/home-1/Team";
 import Testimonials from "@/components/homes/home-1/Testimonials";
 import Link from "next/link";
 import Testimonials2 from "./Teatimonials2";
 import Brands2 from "./Brands2";
-import dynamic from "next/dynamic";
 import Image from "next/image";
 import ParallaxContainer from "@/components/common/ParallaxContainer";
 export default function Home1({ onePage = false, dark = false }) {
